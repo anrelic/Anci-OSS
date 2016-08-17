17d6c84cb4469377974c40970dce297f0c101e3a
Jamefrus <jamefrus@ya.ru>
2016-08-17 23:12:57 +0300

    Inline prioritized function

--- event/src/main/kotlin/su/jfdev/anci/event/ListenerUtil.kt
+++ event/src/main/kotlin/su/jfdev/anci/event/ListenerUtil.kt
@@ -2,6 +2,8 @@ package su.jfdev.anci.event
 
 import su.jfdev.anci.event.Prioritized.*
 
-infix fun <T> Priority.listen(listener: (T) -> Unit): (T) -> Unit = object: Prioritized, (T) -> Unit by listener {
+infix inline fun <E> Priority.listen(crossinline listener: (E) -> Unit): (E) -> Unit = object: (E) -> Unit,
+                                                                                               Prioritized {
     override val priority: Priority get() = this@listen
+    override fun invoke(event: E) = listener(event)
 }
