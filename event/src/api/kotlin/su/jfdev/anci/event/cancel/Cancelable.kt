04df3521e85309e6125b36151d5992ab7c9c8586
Jamefrus <jamefrus@ya.ru>
2016-08-18 07:21:43 +0300

    Simplified to single property

--- event/src/api/kotlin/su/jfdev/anci/event/cancel/Cancelable.kt
+++ event/src/api/kotlin/su/jfdev/anci/event/cancel/Cancelable.kt
@@ -2,8 +2,4 @@ package su.jfdev.anci.event.cancel
 
 interface Cancelable {
     val cancelled: Boolean
-    /**
-     * @return false if already cancelled or too late
-     */
-    fun cancel(): Boolean
 }
