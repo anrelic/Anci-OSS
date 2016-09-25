d4b674049e98efdc1bf599fad8d00e71f831aa23
Jamefrus <jamefrus@ya.ru>
2016-09-25 11:45:49 +0300

    Finally was noinline. It's badly! Fixed to inline + add `when not null` and `when null`

--- util/src/main/kotlin/su/jfdev/anci/util/syntax/SyntaxUtils.kt
+++ util/src/main/kotlin/su/jfdev/anci/util/syntax/SyntaxUtils.kt
@@ -1,3 +1,11 @@
 package su.jfdev.anci.util.syntax
 
-infix fun <T> T.finally(block: (T) -> Unit) = apply(block)
+infix inline fun <T> T.finally(block: (T) -> Unit) = apply(block)
+
+infix inline fun <T: Any> T?.`when null`(block: () -> Unit): T? = finally {
+    if (it == null) block()
+}
+
+infix inline fun <T: Any> T?.`when not null`(block: (T) -> Unit): T? = finally {
+    if (it != null) block(it)
+}
