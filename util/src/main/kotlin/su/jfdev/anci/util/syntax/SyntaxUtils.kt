122e2447a1f4ef5d916dd5bd3ad0f94bf389fa86
Jamefrus <jamefrus@ya.ru>
2016-10-04 10:24:20 +0300

    More syntax features, like 'when true', 'when false'

--- util/src/main/kotlin/su/jfdev/anci/util/syntax/SyntaxUtils.kt
+++ util/src/main/kotlin/su/jfdev/anci/util/syntax/SyntaxUtils.kt
@@ -2,10 +2,34 @@ package su.jfdev.anci.util.syntax
 
 infix inline fun <T> T.finally(block: (T) -> Unit) = apply(block)
 
-infix inline fun <T: Any> T?.`when null`(block: () -> Unit): T? = finally {
+infix inline fun <T: Any> T?.`when null`(block: () -> Unit) = finally {
     if (it == null) block()
 }
 
-infix inline fun <T: Any> T?.`when not null`(block: (T) -> Unit): T? = finally {
+infix inline fun <T: Any> T?.`when not null`(block: (T) -> Unit) = finally {
     if (it != null) block(it)
+}
+
+infix inline fun <T: Boolean?> T.`when true`(block: (T) -> Unit): T = finally {
+    if (it == true) block(it)
+}
+
+infix inline fun <T: Boolean?> T.`when false`(block: (T) -> Unit): T = finally {
+    if (it == false) block(it)
+}
+
+inline fun <T> T.`when same`(inclusion: T, block: (T) -> Unit): T = finally {
+    if (it === inclusion) block(it)
+}
+
+inline fun <T> T.`when other`(exclusion: T, block: (T) -> Unit): T = finally {
+    if (it !== exclusion) block(it)
+}
+
+inline fun <T> T.`when is`(inclusion: T, block: (T) -> Unit): T = finally {
+    if (it == inclusion) block(it)
+}
+
+inline fun <T> T.`when is not`(exclusion: T, block: (T) -> Unit): T = finally {
+    if (it != exclusion) block(it)
 }
